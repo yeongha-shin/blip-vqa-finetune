1bb78412c6cac85cd29f0c5310e3594485bf1734
yeongha-shin <titk123456@gmail.com>
2024-05-07 18:19:43 +0900

    [Feat] 20240507 blip+detr combined model

--- tools/blip2/train.py
+++ tools/blip2/train.py
@@ -180,7 +180,7 @@ class CustomModel(torch.nn.Module):
         single_pixel_mask = torch.ones((1, 750, 1333), dtype=torch.float32, device=device)
 
         # inputs = {'pixel_values': upsampled_feature[0].unsqueeze(0), 'pixel_mask': single_pixel_mask}
-        inputs = self.detr_processor(images=image, return_tensors="pt")
+        inputs = self.detr_processor(images=image)
 
         outputs = self.detr_model(**inputs, labels=labels)
 
