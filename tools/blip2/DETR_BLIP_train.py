641fa540413e41f3f6dd979e88f594f7da477a2b
yeongha-shin <titk123456@gmail.com>
2024-05-09 10:55:08 +0900

    [Feat] 20240509 detr train model

--- tools/blip2/DETR_BLIP_train.py
+++ tools/blip2/DETR_BLIP_train.py
@@ -22,10 +22,14 @@ from tqdm.notebook import tqdm
 import numpy as np
 import matplotlib.pyplot as plt
 
-class CocoDetection(torchvision.datasets.CocoDetection):
+#--------------------------------------------------------------------------------------------
+#                                           Dataset
+#--------------------------------------------------------------------------------------------
+
+class VQADataset(torchvision.datasets.CocoDetection):
     def __init__(self, img_folder, blip_processor, detr_processor, train=True):
         ann_file = os.path.join(img_folder, "Data/custom_train.json" if train else "Data/custom_val.json")
-        super(CocoDetection, self).__init__(img_folder, ann_file)
+        super(VQADataset, self).__init__(img_folder, ann_file)
         self.blip_processor = blip_processor
         self.detr_processor = detr_processor
         self.max_length = 128
@@ -33,7 +37,7 @@ class CocoDetection(torchvision.datasets.CocoDetection):
     def __getitem__(self, idx):
         # read in PIL image and target in COCO format
         # feel free to add data augmentation here before passing them to the next step
-        img, target = super(CocoDetection, self).__getitem__(idx)
+        img, target = super(VQADataset, self).__getitem__(idx)
 
         # preprocess image and target (converting target to DETR format, resizing + normalization of both image and target)
         image_id = self.ids[idx]
@@ -54,8 +58,8 @@ class CocoDetection(torchvision.datasets.CocoDetection):
 blip_processor = AutoProcessor.from_pretrained("Salesforce/blip2-opt-2.7b")
 detr_processor = DetrImageProcessor.from_pretrained("facebook/detr-resnet-50")
 
-train_dataset = CocoDetection(img_folder='./', blip_processor=blip_processor, detr_processor=detr_processor, train=True)
-val_dataset = CocoDetection(img_folder='./', blip_processor=blip_processor, detr_processor=detr_processor, train=False)
+train_dataset = VQADataset(img_folder='./', blip_processor=blip_processor, detr_processor=detr_processor, train=True)
+val_dataset = VQADataset(img_folder='./', blip_processor=blip_processor, detr_processor=detr_processor, train=False)
 
 print("Number of training examples:", len(train_dataset))
 print("Number of validation examples:", len(val_dataset))
